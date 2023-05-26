23198ec28e95c453691a652c10960e404eea1ff4
TbartocciDev <tommybartocci@gmail.com>
2023-05-26 15:40:04 -0400

    navigation bar based content added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      React App
+      <NavBar />
     </div>
   );
 }
---
+++ src/NavBar/NavBar.jsx
@@ -0,0 +1,20 @@
+import "./NavBar.css"
+
+export default function NavBar() {
+    return (
+        <main className="NavBar-main">
+            <div className="links-div">
+                <h2>About</h2>
+                <h2>Portfolio</h2>
+                <h2>Skills</h2>
+                <h2>Contact</h2>
+            </div>
+            <h1>Tommy Bartocci</h1>
+            <div className="socials-div">
+                <h2>Insta</h2>
+                <h2>Linked</h2>
+                <h2>Github</h2>
+            </div>
+        </main>
+    )
+}
