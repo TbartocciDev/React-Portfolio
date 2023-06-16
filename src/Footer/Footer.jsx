9c94a9d899be0b92b815f8538ea2c8bdbcecb1dd
TbartocciDev <tommybartocci@gmail.com>
2023-06-16 00:42:18 -0400

    font size bugs

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -3,7 +3,8 @@ import "./Footer.css"
 export default function Footer() {
     return (
         <main className="Footer-main">
-            <p>Copyright &#169; 2023 | Designed and coded by Thomas Bartocci</p>
+            <p>Copyright &#169; 2023 | Designed and coded by</p>
+            <p>Thomas Bartocci</p>
         </main>
     )
 }
